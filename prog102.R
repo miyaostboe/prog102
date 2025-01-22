5d7f1644a2d0b3a31c447dedab42765cffe9c1a8
miyaostboe <miyaostboe@ucsb.edu>
2025-01-21 19:30:30 -0800

    Initial commit

---
+++ prog102.R
@@ -0,0 +1,54 @@
+library(marinecs100b)
+
+
+# Writing a utility function ----------------------------------------------
+
+# P1: How did you extract the temperature and exposure from the hottest day?
+# Copy-paste the code here.
+
+# P2: Fill in the blanks below to write the Alaskan datetime utility function.
+??? <- ???(???) {
+  ???
+  return(???)
+}
+
+# Extracting data ---------------------------------------------------------
+
+# P3: Make a copy of your code from P1 and edit it to plot the temperature and
+# exposure for "Aialik" on 2012-06-01
+
+# P4: Make a copy of your code from P3 and edit it to plot the temperature and
+# exposure for "Harris" on 2016-04-05.
+
+# P5: Compare your solutions for P3 and P4 - what variables changed?
+
+# P6: What you would pick for the temperature extraction function and
+# parameters' names?
+
+# Writing extraction functions --------------------------------------------
+
+# P7: Fill in the blanks in the code below to write your temperature extraction
+# function.
+
+??? <- ???(???, ???, ???) {
+  ??? <- your_utility_function_name_here(???)
+  ??? <- your_utility_function_name_here(???)
+  ???
+  return(???)
+}
+
+# P8: Make a copy of your solution to P7, and edit it to create exposure and
+# datetime extraction functions.
+
+# P9: Export your annotated screenshot as a JPEG called "annotated_function.jpg"
+# and add it to your copy of the module repository. (It should be in the same
+# folder as this file.)
+
+# P10: Visualize Nuka Pass on July 1, 2018.
+
+# P11: Save a copy of the Nuka Pass plot as "nuka_pass_2018-07-01.png" in this
+# repo
+
+# P12: Compare the code you wrote to create the plot in this module to the code
+# you wrote in PROG101. Qualitatively, how do they compare? Which one is easier
+# to read and why?
